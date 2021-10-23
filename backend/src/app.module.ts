bae0830fe550e39565be6e06d628124ae4b03f74
bibs07 <52453292+bibs07@users.noreply.github.com>
2021-10-23 17:51:24 +1100

    typeorm init

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
 import { CategoryModule } from './categories/category.module';
+import { Category } from './categories/models/category.model';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      port: 3306,
+      host: 'localhost',
+      username: 'root',
+      password: '',
+      database: 'blogapp',
+      synchronize: true,
+      entities: [Category],
+    }),
     CategoryModule,
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
--- backend/src/categories/models/category.model.ts
+++ backend/src/categories/models/category.model.ts
@@ -1,10 +1,14 @@
 import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
-@ObjectType()
+@ObjectType({ description: 'category' })
+@Entity()
 export class Category {
   @Field((_) => Int)
+  @PrimaryGeneratedColumn()
   id: number;
 
   @Field()
+  @Column()
   category: string;
 }
